f3e75bf0d8c0589623ec88412e66de4bdbcab41f
anie1337 <dison2233@126.com>
2021-11-09 23:47:36 +0800

    Update (贪心+二分)-最大值最小化或最小值最大化.cpp

--- (贪心+二分)-最大值最小化或最小值最大化.cpp
+++ (贪心+二分)-最大值最小化或最小值最大化.cpp
@@ -4,49 +4,59 @@
 // 原题: https://www.luogu.com.cn/problem/P1824
 
 typedef unsigned long long ll;
-const int N = 1e6+10;
+const int N = 1e6 + 10;
 
 using namespace std;
 
 ll n, c;
 ll arr[N];
 
-inline bool check(int x) {
+inline bool check(int x)
+{
 	int last = arr[1], count = 1;
-	for(int i = 2; i <= n; i++) {
-		if(arr[i] - last > x){
-	    	count++;
-	    	last=arr[i];
+	for (int i = 2; i <= n; i++)
+	{
+		if (arr[i] - last > x)
+		{
+			count++;
+			last = arr[i];
 		}
-		
-		if(count >= c) {
+
+		if (count >= c)
+		{
 			return true;
 		}
 	}
-	
+
 	return false;
 }
 
-int main() {
+int main()
+{
 	cin >> n >> c;
-	for(int i = 1; i <= n; i++) {
+	for (int i = 1; i <= n; i++)
+	{
 		cin >> arr[i];
 	}
-	
+
 	sort(arr + 1, arr + 1 + n);
-	
+
 	ll left = 1, right = arr[n];
-	while(left < right) {
-		ll mid = (left+right)>>1;
-		
-		if(check(mid)) {
+	while (left < right)
+	{
+		ll mid = (left + right) >> 1;
+
+		if (check(mid))
+		{
 			left = mid + 1;
-		} else {
+		}
+		else
+		{
 			right = mid;
 		}
 	}
-	
+
 	cout << left;
-	
+
 	return 0;
 }
